4ed450539a5ad90bb73143892f610f5f469b5c52
Nithin Ravi <nithinravi2bcommb@gmail.com>
2025-06-18 13:16:04 +0530

    added docker in tools I use section

--- assets/assets.js
+++ assets/assets.js
@@ -35,6 +35,7 @@ import right_arrow from './right-arrow.png';
 import send_icon from './send-icon.png';
 import right_arrow_bold from './right-arrow-bold.png';
 import right_arrow_bold_dark from './right-arrow-bold-dark.png';
+import docker from './docker.png'
 
 export const assets = {
     user_image,
@@ -73,31 +74,9 @@ export const assets = {
     right_arrow,
     send_icon,
     right_arrow_bold,
-    right_arrow_bold_dark
+    right_arrow_bold_dark,
+    docker
 };
-
-// export const workData = [
-//     {
-//         title: 'Frontend project',
-//         description: 'Web Design',
-//         bgImage: '/work-1.png',
-//     },
-//     {
-//         title: 'Geo based app',
-//         description: 'Mobile App',
-//         bgImage: '/work-2.png',
-//     },
-//     {
-//         title: 'Photography site',
-//         description: 'Web Design',
-//         bgImage: '/work-3.png',
-//     },
-//     {
-//         title: 'UI/UX designing',
-//         description: 'UI/UX Design',
-//         bgImage: '/work-4.png',
-//     },
-// ]
 
 export const workData = [
   {
@@ -184,5 +163,5 @@ export const infoList = [
 
 
 export const toolsData = [
-    assets.vscode, assets.firebase, assets.mongodb, assets.figma, assets.git
+    assets.vscode, assets.firebase, assets.mongodb, assets.figma, assets.git, assets.docker
 ];
